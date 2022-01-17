8662110f9132f9a35ac31b5940ec4b64ff04c421
Shaymaa Nagud <snagud21@gmail.com>
2022-01-17 11:57:10 -0500

    commit tree b166571e9629a2739eace87fe3e5c1354fabb48b

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
+function scuberGreetingForFeet(ride){
+ console.log('ride', ride)
+ if (ride < 400){
+   return'This one is on me!'
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+ } else if (ride > 2500) {
+   return 'No can do.'
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+ } else if (ride > 2000) {
+ return 'I will gladly take your thirty bucks.'}
+}
+// if (condition1) {
+//   //  block of code to be executed if condition1 is true
+// } else if (condition2) {
+//   //  block of code to be executed if the condition1 is false and condition2 is true
+// } else {
+//   //  block of code to be executed if the condition1 is false and condition2 is false
+// }
+function ternaryCheckCity(city){
+  return city === 'NYC'?'Ok, sounds good.' : 'No go.';
+
+}
+function switchOnCharmFromTip(tip){
+  if (tip === "generous"){
+    return 'Thank you so much.';
+  }
+ else if (tip === "not as generous"){
+   return "Thank you.";
+ }else {
+   return "Bye."
+ }
 }
